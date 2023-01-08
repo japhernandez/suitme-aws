72f9858e488013a787da20c00925596576c4cd39
John Piedrahita <citymanager02@gmail.com>
2023-01-08 01:18:00 +0100

    refactor

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -3,7 +3,7 @@ resource "aws_iam_role" "app_task_execution_role" {
 
   assume_role_policy = <<EOF
 {
-  "Version": "2012-10-17,
+  "Version": "2012-10-17",
   "Statement": [
     {
       "Sid": "",
